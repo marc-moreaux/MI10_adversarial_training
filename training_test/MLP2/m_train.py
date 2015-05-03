61d165963b5abd25722de01c1c19a097ccf5c8b0
marc-moreaux <mr.moreaux@gmail.com>
2015-05-03 19:29:17 +0200

    remove test from train

--- training_test/MLP2/m_train.py
+++ training_test/MLP2/m_train.py
@@ -11,29 +11,6 @@ def big_print(message):
     print "********************************************************\n"
 
 
-def adv_learning(learning_eps, training_eps):
-
-    # Do some prints
-    # sys.stdout = open("./out"+str(learning_eps)+"_"+str(training_eps)+".txt", "w")
-    msg = "Compute adversarial dataset => " + str(training_eps)
-    big_print(msg)
-    
-    # Compute adversarial dataset
-    preproc.make_adv_training_set(learning_eps, training_eps)
-    draw_images.draw_mnist("mnist_"+str(learning_eps)+"_"+str(training_eps)+".png")
-
-    # Test the model on adversarial dataset
-    mlp_test_yaml = open('mlp_test.yaml', 'r').read()
-    mlp_hyper_params = {'training_eps': training_eps,
-                        'learning_eps': learning_eps,
-                        'save_path': '.'}
-    mlp_test_yaml = mlp_test_yaml % mlp_hyper_params
-    train_obj = yaml_parse.load(mlp_test_yaml)
-    train_obj.main_loop()
-
-
-
-
 #
 # Kind of main thingy
 #
